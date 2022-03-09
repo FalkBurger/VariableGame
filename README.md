3a8cbdac3a592416cf7be7440fe43a41f6652291
FalkBurger <falkburger1@gmail.com>
2022-03-09 15:45:43 +0200

    Figured out the point of the game

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # VariableGame
 A game where the only concept I have for it is that it should involve variables
+
+Strength(Str) determines how much damage physical attacks do as well as your health
+Dexterity(Dex) determines how well you dodge attacks and make ranged attacks
+Willpower(Will) determines how much magic you can cast as well as what level spells you can use
+Intelligence(Int) determines how well your spells perform and modifies magical attacks
--- Variable game.cs
+++ Variable game.cs
@@ -6,6 +6,7 @@ namespace Variables
 	class Program
 	{
 		private static string username, password;
+		private static int strength = 10, dexterity = 10, will = 10, inteligence = 10;
 		private static int inputNumber = 0;
 		
 		static void Main(string[] args)
@@ -17,11 +18,63 @@ namespace Variables
 		
 		static void MainMenu()
 		{
+			bool doLoop = true;
+			while (doLoop)
+			{
+				Console.Clear();			
+				Console.WriteLine("Variable Game Menu\n");	
+				Console.WriteLine("1) View Statistics");		
+				Console.WriteLine("2) Save Profile");
+				Console.WriteLine("3) Exit game");		
+				Console.Write(">> ");
+				
+				try
+				{
+					inputNumber = Convert.ToInt32(Console.ReadLine());
+					if ((inputNumber > 0) || (inputNumber < 3))
+						doLoop = true;
+					else
+						Console.Clear();
+				}
+				catch
+				{
+					Console.Clear();
+				}
+
+				switch (inputNumber)
+				{
+					case 1:
+						ViewStats();
+						break;
+
+					case 2:
+						SaveGame();
+						break;
+					
+					case 3:
+						doLoop = false;
+						break;
+				}
+			}	
+		}
+
+		static void ViewStats()
+		{
 			Console.Clear();
-			
-			Console.WriteLine("Variable Game Menu\n");
-			
-			Console.WriteLine("1) Save Profile");
+			Console.WriteLine("Your character's stats. Refer to readme for how they work\n");
+
+			Console.WriteLine($"Str: {strength}");
+			Console.WriteLine($"Dex: {dexterity}");
+			Console.WriteLine($"Will: {will}");
+			Console.WriteLine($"Int: {inteligence}");
+			Console.ReadLine();
+		}
+
+		static void SaveGame()
+		{
+			string outputText = $"{password}\n{strength}\n{dexterity}\n{will}\n{inteligence}";
+
+			File.WriteAllText($"{username}.txt",outputText);
 		}
 		
 		static void StartScreen()
@@ -94,10 +147,32 @@ namespace Variables
 			foreach (string line in File.ReadLines($"{readUsername}.txt"))
 			{
 				fileReader[counter] = line;
+
+				switch (counter)
+				{
+					case 0:			
+						password = fileReader[counter];
+						break;
+
+					case 1:
+						strength = Convert.ToInt32(fileReader[counter]);
+						break;
+
+					case 2:
+						dexterity = Convert.ToInt32(fileReader[counter]);
+						break;
+
+					case 3:
+						will = Convert.ToInt32(fileReader[counter]);
+						break;
+
+					case 4:
+						inteligence = Convert.ToInt32(fileReader[counter]);
+						break;
+				}
+
 				counter++;
 			}
-			
-			password = fileReader[0];
 			
 			if (readPassword != password)
 			{
